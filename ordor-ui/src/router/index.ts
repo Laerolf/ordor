bf9b868f4a4dbfb3fa3afa56b41040f2a5f8fb58
Nicolas Laerolf <nicolas.floreal@hotmail.com>
2024-05-13 19:18:04 +0900

    :sparkles: setup UI project

---
+++ ordor-ui/src/router/index.ts
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'menu',
+      component: () => import('@/views/MenuView.vue')
+    }
+  ]
+})
+
+export default router
